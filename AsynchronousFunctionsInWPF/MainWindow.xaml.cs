495dd60a66e50582f5b2a35a6c01025d5bf07d32
Michael Beck <mbeck@lutz-und-grub.de>
2020-04-09 11:50:37 +0200

    asynchroner Webzugriff

--- AsynchronousFunctionsInWPF/MainWindow.xaml.cs
+++ AsynchronousFunctionsInWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -22,19 +23,51 @@ namespace AsynchronousFunctionsInWPF
     public partial class MainWindow : Window
     {
         Button button = new Button { Content = "Go" };
+        Button button2 = new Button { Content = "Web" };
         TextBlock result = new TextBlock();
         public MainWindow()
         {
             InitializeComponent();
             var stackpanel = new StackPanel();
             stackpanel.Children.Add(button);
+            stackpanel.Children.Add(button2);
             stackpanel.Children.Add(result);
             this.Content = stackpanel;
             button.Click += (sender, args) => Go();
+            button2.Click += (sender, args) => Go2();
         }
+
+        async void Go2()
+        {
+            button2.IsEnabled = false;
+            result.Text = "";
+            string[] urls = "www.tagesschau.de www.microsoft.com www.google.de www.apple.com www.heise.de www.facebook.com www.lutzundgrub.de".Split();
+            int totalLength = 0;
+            try
+            {
+                foreach(string url in urls)
+                {
+                    var uri = new Uri("https://" + url);
+                    //byte[] data = new WebClient().DownloadData(uri); //Synchron
+                    byte[] data = await new WebClient().DownloadDataTaskAsync(uri); //Asynchron
+                    result.Text += "Größe von " + url + " ist " + data.Length + Environment.NewLine;
+                    totalLength += data.Length;
+                }
+                result.Text += "Gesamtgröße: " + totalLength;
+            }catch (WebException ex)
+            {
+                result.Text += "Fehler: " + ex.Message;
+            }
+            finally
+            {
+                button2.IsEnabled = true;
+            }
+        }
+
 
         async void Go()
         {
+            result.Text = "";
             button.IsEnabled = false;
             for (int i = 1; i < 5; i++)
             {
