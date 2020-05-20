86fae3a09e7e57096be2417a2b46f5c39f230842
mbeck-tr <mbeck@lutz-und-grub.de>
2020-05-20 13:32:33 +0200

    post verb

--- JavaScriptModul/expresswebserver/main.js
+++ JavaScriptModul/expresswebserver/main.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+var formidable = require('formidable');
 //Http Verben:
 //Http-Get  --> Anfrage an den Server
 //Http-Post --> Datenübermittlung an den Server
@@ -16,6 +16,18 @@ app.get("/SubmitHello", function (anfrage,antwort){
     antwort.write('Hallo ' + anfrage.query.BenutzerName + "!<br/>");
     antwort.end("Have a great day!");
     console.log("Handled request from " + anfrage.query.BenutzerName);
+});
+
+app.post("/PostHello", function(anfrage,antwort){
+    if (anfrage.method.toLowerCase() == 'post'){
+        var form = formidable.IncomingForm();
+        form.parse(anfrage, function (err, attribute){
+            antwort.writeHead(200, {'Content-Type': 'text/html'});
+            antwort.write('Hello ' + attribute.BenutzerName + "!<br/>");
+            antwort.end("Have a POST great day!");
+            console.log("Handled request from " + attribute.BenutzerName);
+        });
+    }
 });
 
 var port = 8000;
